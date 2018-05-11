1c723792d140b5c94856d49258db3bad98391060
chrelmi <christian.elmi@gmail.com>
2018-05-11 21:57:53 +0200

    versione 1.1

--- interface.py
+++ interface.py
@@ -3,13 +3,15 @@ import tkinter
 from tkinter import *
 window=Tk()
 window.title("Stazione Meteo")
-window.geometry("700x600")
+window.geometry("800x400")
 window.configure(background="#000000")
 l=tkinter.Label(window,text="Perugia",bg="#000000",fg="#FFFFFF")
-l.pack()
-img=tkinter.PhotoImage(file="sereno.png")
+l3=tkinter.Label(window, text="Scattered clouds   20°C", bg="#000000", fg="#FFFFFF")
+l3.config(font=("tlwgmono",22))
+l.config(font=("verdana", 44))
+l.grid(row=0,column=0, padx=50)
+l3.grid(row=1,column=0, padx=20)
+img=tkinter.PhotoImage(file="rain.png")
 l2=tkinter.Label(window,image=img,bg="#000000")
-l2.pack()
-
-
+l2.grid(row=0, column=1, padx=100, rowspan=2, pady=40)
 window.mainloop()
--- meteo.py
+++ meteo.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import requests
+import time
 url='http://api.openweathermap.org/data/2.5/weather?appid=39411ec0a72cb5c7b070e9da15555979&q='
 city=url+"perugia, IT"
 jdata=requests.get(city).json()
@@ -10,4 +11,15 @@ fd2=int(fd2)
 fd2=str(fd2)
 print(jdata)
 print("-------------------------------------------------")
+time.sleep(5)
 print(fd+" "+fd2)
+
+
+
+
+
+"""
+sfruttare time.sleep(ogni 3 ore) e mandare il programma in loop per evitare di usare il corntab-
+integrare la grfica qui
+cambiare l'aspetto delle scritte
+"""
